4d8acce06dd4e3d851758b0dd1b9fa7bf4790379
Boston Lowrey <bostonlowrey@gmail.com>
2023-05-22 13:35:45 -0400

    user can login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,20 @@ class UsersController < ApplicationController
     redirect_to root_path
   end
 
+  def login_form
+  end
+
+  def login
+    user = User.find_by(username: params[:username])
+    if user.authenticate(params[:password])
+      flash[:success] = "Welcome, #{user.username}!"
+      redirect_to root_path
+    else
+      flash[:error] = "Sorry, your credentials are bad."
+      render :login_form
+    end
+  end
+
   private
   def user_params
     params.require(:user).permit(:username, :password)
